0d23c70cc24fa957d697659d7c35d9ef0c61ba26
Viet Hoang <1300077+vietqhoang@users.noreply.github.com>
2022-04-28 14:41:23 -0700

    Add Github link

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,21 @@ import * as React from "react";
 
 import PlateCalculator from "../components/plate-calculator/plate-calculator";
 
-const IndexPage = () => <PlateCalculator />;
+const IndexPage = () => {
+  return (
+    <div className="relative">
+      <PlateCalculator />
+
+      <div className="absolute bottom-0 right-0 m-4">
+        <a
+          className="inline-block p-4 rounded bg-slate-200 hover:bg-slate-300 text-slate-700 font-medium"
+          href="https://www.github.com/vietqhoang/fortyfives"
+        >
+          Github
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default IndexPage;
